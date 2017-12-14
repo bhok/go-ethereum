6d67bb937704dd7d0efbd67fff5e4f83f0710c34
bhok <bhokor@gmail.com>
2017-12-14 01:37:00 +0100

    Update main.go

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -108,6 +108,7 @@ var (
 		utils.DeveloperPeriodFlag,
 		utils.TestnetFlag,
 		utils.RinkebyFlag,
+		utils.CosmosFlag,
 		utils.VMEnableDebugFlag,
 		utils.NetworkIdFlag,
 		utils.RPCCORSDomainFlag,
